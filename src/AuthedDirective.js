ae04043090e22086cd0b4b9cad923fc25b1e0c9b
Andrew Short <andrewjshort@gmail.com>
2019-05-17 23:15:03 +1000

    Required authed

---
+++ src/AuthedDirective.js
@@ -0,0 +1,17 @@
+import { AuthenticationError, SchemaDirectiveVisitor } from 'apollo-server';
+import { defaultFieldResolver } from 'graphql';
+
+export default class AuthedDirective extends SchemaDirectiveVisitor {
+  visitFieldDefinition(field) {
+    const resolve = field.resolve || defaultFieldResolver;
+
+    field.resolve = (source, args, context, info) => {
+      if (!context.member) {
+        throw new AuthenticationError('You must be logged in');
+      }
+
+
+      return resolve.apply(this, [source, args, context, info]);
+    };
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer, gql, AuthenticationError } from 'apollo-server';
 import dotenv from 'dotenv';
 import { readFileSync } from 'fs';
+import jwt from 'jsonwebtoken';
 import { MongoClient } from 'mongodb';
 import path from 'path';
 
+import AuthedDirective from './AuthedDirective';
 import MembersDatabase from './datasources/MembersDatabase';
 import resolvers from './resolvers';
 
@@ -13,13 +15,40 @@ const schema = readFileSync(path.join(__dirname, 'schema.gql'), 'utf-8');
 const typeDefs = gql(schema);
 
 const mongo = new MongoClient(process.env.MONGODB_URL, { useNewUrlParser: true });
-const db = process.env.MONGODB_DB;
+const membersDatabase = new MembersDatabase(mongo, process.env.MONGODB_DB);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async (context) => {
+    const auth = context.req.headers.authorization;
+
+    if (!auth) {
+      return context;
+    }
+
+    const parts = auth.split(' ');
+
+    if (parts.length < 2 || parts[0].toLowerCase() !== 'bearer') {
+      return context;
+    }
+
+    const token = parts.slice(1).join(' ');
+
+    try {
+      const payload = jwt.verify(token, process.env.JWT_SECRET);
+      const member = await membersDatabase.fetchMember(payload.number);
+
+      return { member, ...context };
+    } catch (err) {
+      throw AuthenticationError('Invalid token');
+    }
+  },
+  schemaDirectives: {
+    authed: AuthedDirective,
+  },
   dataSources: () => ({
-    members: new MembersDatabase(mongo, db),
+    members: membersDatabase,
   }),
 });
 
