2db996946866b66274dadb65d4fd51feb783817e
Andrew Short <andrewjshort@gmail.com>
2019-05-17 21:18:16 +1000

    Fetch members from live database

--- src/datasources/MembersDatabase.js
+++ src/datasources/MembersDatabase.js
@@ -1,16 +1,31 @@
 import { DataSource } from 'apollo-datasource';
 
+function transformMember({ _id, ...record }) {
+  const names = record.Name.split(' ');
+
+  return {
+    _id,
+    number: parseInt(record.Id, 10),
+    fullName: record.Name,
+    givenNames: names.slice(0, -1).join(' '),
+    surname: names[names.length - 1],
+    team: record.Team,
+  };
+}
+
 export default class MembersDatabase extends DataSource {
   constructor(client, db) {
     super();
+
     this.db = client.connect().then(connection => connection.db(db));
+    this.members = this.db.then(connection => connection.collection('members'));
   }
 
   fetchMembers() {
-    return this.db.then(db => db.collection('members').find().toArray());
+    return this.members.then(members => members.find().map(transformMember).toArray());
   }
 
   fetchMember(number) {
-    return this.db.then(db => db.collection('members').findOne({ number }));
+    return this.db.then(db => db.collection('members').findOne({ number }).then(this.transformMember));
   }
 }
