316a6cab6a3d6657c0a334d54be22d226a53e6dc
Andrew Short <andrewjshort@gmail.com>
2019-05-17 21:35:12 +1000

    Date time types

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,4 +1,8 @@
+import { GraphQLDate, GraphQLDateTime } from 'graphql-iso-date';
+
 export default {
+  Date: GraphQLDate,
+  DateTime: GraphQLDateTime,
   Query: {
     members: (_obj, _args, { dataSources }) => dataSources.members.fetchMembers(),
     member: (_obj, { number }, { dataSources }) => dataSources.members.fetchMember(number),
