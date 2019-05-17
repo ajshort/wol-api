d0accd96a414d0b8c54373771343c79eef574df1
Andrew Short <andrewjshort@gmail.com>
2019-05-17 16:21:52 +1000

    Schema in gql file

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 import { ApolloServer, gql } from 'apollo-server'
+import { readFileSync } from 'fs'
+import path from 'path'
 
-const typeDefs = gql`
-  type Query {
-    hello: String!
-  }
-`
+const schema = readFileSync(path.join(__dirname, 'schema.gql'), 'utf-8')
+const typeDefs = gql(schema)
 
 const resolvers = {
   Query: {
